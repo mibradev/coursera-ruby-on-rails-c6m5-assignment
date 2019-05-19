6d6597d78496377e71dad1ba9b95a7f622afac6b
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2019-05-19 10:57:21 +0200

    Supprt loading an image in signup page

--- app/assets/javascripts/spa-demo/authn/signup/signup.component.js
+++ app/assets/javascripts/spa-demo/authn/signup/signup.component.js
@@ -12,13 +12,14 @@
   templateUrl.$inject = ["spa-demo.config.APP_CONFIG"];
   function templateUrl(APP_CONFIG) {
     return APP_CONFIG.authn_signup_html;
-  }    
+  }
 
-  SignupController.$inject = ["$scope","$state","spa-demo.authn.Authn"];
-  function SignupController($scope, $state, Authn) {
+  SignupController.$inject = ["$scope","$state","spa-demo.authn.Authn", "spa-demo.layout.DataUtils", "spa-demo.subjects.Image"];
+  function SignupController($scope, $state, Authn, DataUtils, Image) {
     var vm=this;
     vm.signupForm = {}
     vm.signup = signup;
+    vm.setImageContent = setImageContent;
 
     vm.$onInit = function() {
       console.log("SignupController",$scope);
@@ -31,15 +32,18 @@
       Authn.signup(vm.signupForm).then(
         function(response){
           vm.id = response.data.data.id;
-          console.log("signup complete", response.data, vm);          
+          console.log("signup complete", response.data, vm);
           $state.go("home");
         },
         function(response){
           vm.signupForm["errors"]=response.data.errors;
-          console.log("signup failure", response, vm);          
+          console.log("signup failure", response, vm);
         }
       );
     }
 
+    function setImageContent(dataUri) {
+      vm.image_content = DataUtils.getContentFromDataUri(dataUri);
+    }
   }
 })();
