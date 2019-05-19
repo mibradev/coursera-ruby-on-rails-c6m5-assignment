a491cd15ec9a4e076a3cc94130d173a92b3e55dd
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2019-05-19 07:49:17 +0200

    Do not list user images

--- app/models/image.rb
+++ app/models/image.rb
@@ -6,6 +6,8 @@ class Image < ActiveRecord::Base
   has_many :thing_images, inverse_of: :image, dependent: :destroy
   has_many :things, through: :thing_images
 
+  scope :except_user_images, -> { joins("LEFT OUTER JOIN users ON images.id = users.image_id").where("users.image_id": nil) }
+
   def basename
     caption || "image-#{id}"
   end
